d99e6de3eaf75b848b8f239af32cb25c295abaa7
jirayu <jirayu004za@gmail.com>
2023-02-24 05:21:19 +0700

    Add methods but not at all

--- src/Back-end/NodeFactory.java
+++ src/Back-end/NodeFactory.java
@@ -1,2 +1,60 @@
+import java.util.*;
 public class NodeFactory {
+    private static NodeFactory instance;
+    private NodeFactory(){}
+
+    public static NodeFactory instance(){
+        if (instance == null) {
+            instance = new NodeFactory();
+        }
+        return instance;
+    }
+
+    //return BlockStatementNode
+    public BlockStatementNode createBlockStatementNode(){
+        return new BlockStatementNode();
+    }
+
+    // create a new assignment statement node
+    public Node createNode(VarNode varNode, Node expressionNode){
+        return new AssignmentStatementNode(varNode, expressionNode);
+    }
+
+    // create a new move command node
+    public MoveCommandNode createMoveCommandNode(String direction, CityCrew city){
+        return new MoveCommandNode(direction, city);
+    }
+
+    // create a new attack command node
+    public AttackCommandNode createAttackCommandNode(String direction, CityCrew city){
+        return new AttackCommandNode(direction, city);
+    }
+
+    // create a new while statement node
+    public Node createNode(Node expressionNode, Node statementNode){
+        return new WhileStatementNode(expressionNode, statementNode);
+    }
+
+    // create a new if statement node
+    public Node createIfStatementNode(Node expressionNode, Node ifTrueStatementNode, Node ifFalseStatementNode){
+        return new IfStatementNode(expressionNode, ifTrueStatementNode, ifFalseStatementNode);
+    }
+
+    // create a new binary arithmetic node
+    public Node createNode(Node left, String op, Node right){
+        return new BinaryArithmeticNode(left, op, right);
+    }
+
+    // create a new variable node
+    public VarNode createVarNode(String identifier, Map<String,Double> var){
+        return new VarNode(identifier, var);
+    }
+
+    // create a new number node
+    public NumberNode createNumberNode(double number){
+        return new NumberNode(number);
+    }
+
+
+
 }
