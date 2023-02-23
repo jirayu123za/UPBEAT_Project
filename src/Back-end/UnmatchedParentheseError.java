f44a3ce87c6190df015931e763a16227b92f87e1
jirayu <jirayu004za@gmail.com>
2023-02-24 00:10:43 +0700

    delete class

--- src/Back-end/UnmatchedParentheseError.java
+++
@@ -1,8 +0,0 @@
-public class UnmatchedParentheseError {
-
-    public class UnmatchedParenthesesError extends SyntaxError{
-        public UnmatchedParenthesesError(String message) {
-          super(message);
-        }
-    }
-}
