0afef4bda1d19b843921c2d80e4b7fb6f45bf2c4
jirayu <jirayu004za@gmail.com>
2023-02-24 04:25:54 +0700

    Add class

---
+++ src/node/Node/OpponentNode.java
@@ -0,0 +1,17 @@
+public class OpponentNode implements Node{
+    protected CityCrew city;
+
+    public OpponentNode(CityCrew city){
+        this.city = city;
+    }
+
+    @Override
+    public double evaluate() {
+        return city.getOpponentLocation();
+    }
+
+    @Override
+    public void print(int height) {
+
+    }
+}
