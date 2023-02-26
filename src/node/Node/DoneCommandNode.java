b3b766d945ce40bb7ec27a15b407ed90ca22211a
jirayu <jirayu004za@gmail.com>
2023-02-26 07:40:36 +0700

    implement class

--- src/node/Node/DoneCommandNode.java
+++ src/node/Node/DoneCommandNode.java
@@ -1,4 +1,10 @@
 public class DoneCommandNode implements Node{
+    protected CityCrew city;
+
+    public DoneCommandNode(CityCrew city){
+            this.city = city;
+    }
+
     @Override
     public double evaluate() {
         return 0;
