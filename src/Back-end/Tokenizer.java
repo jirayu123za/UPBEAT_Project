a37cebe7d772e461ae70760a9a9121574a33d419
jirayu <jirayu004za@gmail.com>
2023-02-26 07:58:10 +0700

    implement Tokenizer class

--- src/Back-end/Tokenizer.java
+++ src/Back-end/Tokenizer.java
@@ -1,2 +1,42 @@
+import java.util.*;
+
 public class Tokenizer {
+    protected List<String> tokens;
+    protected String next;
+
+    public Tokenizer() throws SyntaxError{
+        tokens = new ArrayList<>();
+        computeNext();
+    }
+
+    private void computeNext(){
+        /*need implement*/
+    }
+
+    private String peek(){
+        return next;
+    }
+
+    private String consume(){
+        String result = next;
+        computeNext();
+
+        return result;
+    }
+
+    boolean peek(String r){
+        if(peek() != null)
+            return peek().matches(r);
+        return false;
+    }
+
+    void consume(String r) throws SyntaxError{
+            if(peek().matches(r)){
+                consume();
+            }else {
+                throw new SyntaxError("consume(r): token does not match r");
+            }
+    }
+
+
 }
