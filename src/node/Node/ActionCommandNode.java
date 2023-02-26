2cb2d4a1fe43f0133583c36a4e55d33969c91adc
jirayu <jirayu004za@gmail.com>
2023-02-26 07:34:01 +0700

    implement but no complete

--- src/node/Node/ActionCommandNode.java
+++ src/node/Node/ActionCommandNode.java
@@ -1,6 +1,16 @@
 public class ActionCommandNode implements Node{
+    protected String action;
+    protected CityCrew city;
+
+    public ActionCommandNode(String action, CityCrew city){
+        this.action = action;
+        this.city = city;
+    }
+
+
     @Override
     public double evaluate() {
+        /*Implement*/
         return 0;
     }
 
