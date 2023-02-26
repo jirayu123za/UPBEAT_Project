2b50032b8a8f84ec6bc39889e20af5f929d32321
jirayu <jirayu004za@gmail.com>
2023-02-26 23:07:50 +0700

    create field

---
+++ src/Back-end/RegularExpression.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class RegularExpression {
+    // String direction
+    public static final String DIRECTION_REGEX = " up | down | upleft | upright | downleft | downright";
+    public static final String UP_REGEX = "";
+    public static final String DOWN_REGEX = "";
+    public static final String UPLEFT_REGEX = "";
+    public static final String UPRIGHT_REGEX = "";
+    public static final String DOWNLEFT_REGEX = "";
+    public static final String DOWNRIGHT_REGEX = "";
+
+    // String reserved words
+    public static final String RESERVEDWORD_REGEX = "collect|done|down|downleft|downright|else|if|invest|move|nearby|opponent|relocate|shoot|then|up|upleft|upright|while";
+
+    // String infoExpression, opponent, nearby
+    public static final String INFOEXPRESSION_REGEX = "";
+    public static final String OPPONENT_REGEX = "";
+    public static final String NEARBY_REGEX = "";
+
+    // String action, done, relocate, invest, collect, shoot
+    public static final String ACTION_REGEX = "";
+    public static final String DONE_REGEX = "";
+    public static final String RELOCATE_REGEX = "";
+    public static final String INVEST_REGEX = "";
+    public static final String COLLECT_REGEX = "";
+    public static final String SHOOT_REGEX = "";
+
+    // String if, then, else
+    public static final String IF_REGEX = "";
+    public static final String THEN_REGEX = "";
+    public static final String ELSE_REGEX = "";
+
+    // String while
+    public static final String WHILE_REGEX = "";
+
+    // String operator, assign, parentheses
+    public static final String OPERATOR_REGEX = "";
+    public static final String ASSIGN_REGEX = "";
+    public static final String PARENTHESES_REGEX = "";
+
+    // String number, variable
+    public static final String NUMBER_REGEX = "";
+    public static final String VARIABLE_REGEX = "";
+
+    // String random
+    public static final String RANDOM_REGEX = "";
+
+    // String all
+    public static final String ALL_REGEX = "";
+
+
+
+
+
+
+
+
+
+
+}
