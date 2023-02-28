17f587e3835eb9f70faba2925b59c2bb50731fa2
jirayu <jirayu004za@gmail.com>
2023-03-01 06:08:18 +0700

    implement class

--- Back_end/src/main/Project/Territory.java
+++ Back_end/src/main/Project/Territory.java
@@ -3,10 +3,25 @@ import java.util.*;
 
 public class Territory {
     // Variable
+    protected int m,n;
+    protected long max_deposit;
+    protected long interestRate_percentage;
+    protected Region[][] Territory = null;
 
     // Constructor
+    public Territory(long m, long n, long max_deposit, long interestRate_percentage){
+        this.m = (int) m;
+        this.n = (int) n;
+        this.max_deposit = max_deposit;
+        this.interestRate_percentage = interestRate_percentage;
+        Territory = new Region[this.m][this.n];
 
+        for (int i = 0; i < m; i++){
+            for (int j = 0 ;j < n; j++){
+                Territory[i][j] = new Region(this.max_deposit, this.interestRate_percentage, i, j);
+            }
+        }
+    }
 
-    // method
 
 }
