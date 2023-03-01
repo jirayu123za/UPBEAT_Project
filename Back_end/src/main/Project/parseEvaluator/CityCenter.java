5e830aa5b52298bd91dec7b17ae081609caf6b28
jirayu <jirayu004za@gmail.com>
2023-03-01 18:10:41 +0700

    add file CityCenter class and modify

---
+++ Back_end/src/main/Project/parseEvaluator/CityCenter.java
@@ -0,0 +1,17 @@
+package Project.parseEvaluator;
+
+public class CityCenter {
+    // variable
+    protected int centerRow;
+    protected int centerCol;
+    protected long init_center_deposit;
+
+    // constructor
+    public CityCenter(long centerRow, long centerCol, long init_center_deposit){
+        this.centerRow = (int) centerRow;
+        this.centerCol = (int) centerCol;
+        this.init_center_deposit = init_center_deposit;
+    }
+
+    // method
+}
