25cf7c18d0ed1f960f3da50908b94c1aceaac39d
jirayu <jirayu004za@gmail.com>
2023-03-02 04:41:22 +0700

    implement and modify class, not sure, it corrects

--- Back_end/src/main/Project/Player.java
+++ Back_end/src/main/Project/Player.java
@@ -3,13 +3,127 @@ import java.util.*;
 
 public class Player {
     // Variable
+    protected long init_budget;
+
+    protected String name;
+    protected int playerTurn = 0;
+    protected boolean outOfBudget = false;
+    protected PersonalInfo CityCrew = null;
+    protected PersonalInfo CityCenter = null;
+    protected Region currentRegion;
+    protected Territory territory;
+    protected HashMap<String, Integer> PersonalValue = null;
+    protected ArrayList<PersonalInfo> RegionProcess = new ArrayList<>();
 
     // Constructor
-    public Player(){
-        /**
-         * need to implement
-         * */
+    public Player(String name, long init_budget, long x, long y, Territory territory){
+        this.name = name;
+        this.init_budget = init_budget;
+        this.territory = territory;
+        CityCrew = new PersonalInfo();   // need implement
+        CityCenter = new PersonalInfo(); // need implement
+        PersonalValue = new HashMap<>();
     }
 
     // method
+    public boolean hasEnoughBudget(long amount){
+        return init_budget >= amount;
+    }
+
+    public void decreaseBudget(long amount){
+        init_budget -= amount;
+    }
+
+    public void setOutOfBudget() {
+        outOfBudget = true;
+    }
+
+    public List<Region> getAdjacentRegions(int x, int y){
+        List<Region> adjacentRegions = new ArrayList<>();
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (i >= 0 && i < territory.getRows() && j >= 0 && j < territory.getCols() && !(i == x && j == y)) {
+                    adjacentRegions.add(territory.getRegion(i, j));
+                }
+            }
+        }
+        return adjacentRegions;
+    }
+
+    // Getters and Setters
+    public long getInit_budget() {
+        return init_budget;
+    }
+
+    public void setInit_budget(long init_budget) {
+        this.init_budget = init_budget;
+    }
+
+    public long getBudget() {
+        return init_budget;
+    }
+
+    public void setBudget(long budget) {
+        this.init_budget = budget;
+    }
+
+    public boolean isOutOfBudget() {
+        return outOfBudget;
+    }
+
+    public void setOutOfBudget(boolean outOfBudget) {
+        this.outOfBudget = outOfBudget;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlayerTurn() {
+        return playerTurn;
+    }
+
+    public void setPlayerTurn(int playerTurn) {
+        this.playerTurn = playerTurn;
+    }
+
+    public PersonalInfo getCityCrew() {
+        return CityCrew;
+    }
+
+    public void setCityCrew(PersonalInfo cityCrew) {
+        CityCrew = cityCrew;
+    }
+
+    public PersonalInfo getCityCenter() {
+        return CityCenter;
+    }
+
+    public void setCityCenter(PersonalInfo cityCenter) {
+        CityCenter = cityCenter;
+    }
+
+
+    public Region getCurrentRegion() {
+        return currentRegion;
+    }
+
+    public void setCurrentRegion(Region currentRegion) {
+        this.currentRegion = currentRegion;
+    }
+
+    public Territory getTerritory() {
+        return territory;
+    }
+
+    public void setTerritory(Territory territory) {
+        this.territory = territory;
+    }
+
+
+
 }
--- Back_end/src/main/Project/Region.java
+++ Back_end/src/main/Project/Region.java
@@ -4,6 +4,7 @@ public class Region {
     // Variable
     protected long max_deposit;
     protected long interestRate_percentage;
+    protected long init_budget;
 
     protected double deposit;
     protected int pos_xAxis;
@@ -12,14 +13,23 @@ public class Region {
     protected boolean CityCenter = false;
 
     // Constructor
-    public Region(long interestRate_percentage, long max_deposit, int pos_xAxis, int pos_yAxis){
+    public Region(long interestRate_percentage, long max_deposit,  long init_budget, int pos_xAxis, int pos_yAxis){
         this.pos_xAxis = pos_xAxis;
         this.pos_yAxis = pos_yAxis;
         this.interestRate_percentage = interestRate_percentage;
         this.max_deposit = max_deposit;
+        this.init_budget = init_budget;
     }
 
     // method
+    public int getPos_xAxis(){
+        return pos_xAxis;
+    }
+
+    public int getPos_yAxis(){
+        return pos_yAxis;
+    }
+
     public long Int(int turn){
         return (long) (this.interestRate_percentage*(Math.log10(deposit))*Math.log(turn));
     }
@@ -43,47 +53,66 @@ public class Region {
     }
 
     public void changeCityCenter(boolean change){
-        /**
-         * need to implement
-         * */
+        this.CityCenter = change;
     }
 
-    public void isOwner(String name){
-        /**
-         * need to implement
-         * */
+    public boolean isOwner(String name){
+        return owner != null && owner.equals(name);
     }
 
     public void changeOwner(String owner){
-        /**
-         * need to implement
-         * */
+        this.owner = owner;
     }
 
-    public void invest(){
-        /**
-         * need to implement
-         * */
+    public void invest(int investmentAmount, Player player){
+        int totalCost = investmentAmount + 1;
+
+        if(player.hasEnoughBudget(totalCost)){
+            if(owner == null || owner.equals(player.getName()) || isAdjacentToPlayerRegion(player) ){
+                int newDeposit = (int) Math.min(deposit + investmentAmount, max_deposit);
+                deposit = newDeposit;
+                player.decreaseBudget(totalCost);
+            }
+        }else{
+            player.setOutOfBudget();
+        }
     }
 
-    public boolean collect(){
-        /**
-         * need to implement
-         * */
+    public void collect(int collectionAmount, Player player){
+        int totalCost = 1;
+
+        if(player.hasEnoughBudget(totalCost)){
+            if(collectionAmount <= deposit){
+                deposit -= collectionAmount;
+                player.decreaseBudget(totalCost);
+            }
+        }else{
+            player.setOutOfBudget();
+        }
+    }
+
+    public boolean isAdjacentToPlayerRegion(Player player){
+        for(Region adjacentRegion : player.getAdjacentRegions(this.pos_xAxis, this.pos_yAxis)){
+            if(Math.abs(adjacentRegion.pos_xAxis - this.pos_xAxis) <= 1 && Math.abs(adjacentRegion.pos_yAxis - this.pos_yAxis) <= 1) {
+                if (adjacentRegion.owner != null && adjacentRegion.owner.equals(player.getName())) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
-    public int returnDeposit(){
-        /**
-         * need to implement
-         * */
-        return 0;
+    public int returnDeposit(String name){
+        if(isOwner(name)){
+            return (int) Math.floor(deposit);
+        }else{
+            return ((int) Math.floor(deposit))*-1;
+        }
     }
 
-    public void updateDeposit(){
-        /**
-         * need to implement
-         * */
+    public void updateDeposit(int turn){
+        interestRate_percentage = Int (turn);
+        this.deposit += this.deposit*interestRate_percentage/100;
     }
 
 }
