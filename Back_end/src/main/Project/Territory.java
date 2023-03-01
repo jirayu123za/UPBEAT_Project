5ecc5b407c7e41bc442ddd8640f96f70d3fcf646
jirayu <jirayu004za@gmail.com>
2023-03-02 04:28:57 +0700

    add methods getRegion, getRows, getCols

--- Back_end/src/main/Project/Territory.java
+++ Back_end/src/main/Project/Territory.java
@@ -1,11 +1,11 @@
 package Project;
-import java.util.*;
 
 public class Territory {
     // Variable
     protected int m,n;
     protected long max_deposit;
     protected long interestRate_percentage;
+    protected long init_budget;
     protected Region[][] Territory = null;
 
     // Constructor
@@ -18,10 +18,22 @@ public class Territory {
 
         for (int i = 0; i < m; i++){
             for (int j = 0 ;j < n; j++){
-                Territory[i][j] = new Region(this.max_deposit, this.interestRate_percentage, i, j);
+                Territory[i][j] = new Region(this.max_deposit, this.interestRate_percentage, init_budget, i, j);
             }
         }
     }
 
+    // method
+    public Region getRegion(int i, int j){
+        return Territory[i][j];
+    }
+
+    public int getRows(){
+        return m;
+    }
+
+    public int getCols(){
+        return n;
+    }
 
 }
