d6a3ed3225cc2487d37b457504ef02ae8c616416
jirayu <jirayu004za@gmail.com>
2023-03-12 03:30:56 +0700

    modify constructor and detail on some methods

--- Back_end/src/main/Project/ThisTurn/TurnConfig.java
+++ Back_end/src/main/Project/ThisTurn/TurnConfig.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class TurnConfig implements Turn{
     protected long turn;
     protected Player player1;
+    protected Player player2;
     protected Player currentPlayer;
     protected Map<Player, Region> cityCenterOfRegion;
     protected Configuration config;
@@ -15,12 +16,20 @@ public class TurnConfig implements Turn{
 
 
     // Constructor
-    public TurnConfig(long turn){
+    public TurnConfig(Configuration config, List<Region> territory, Player player1, Player player2, long turn){
+        this.config = config;
+        this.territory = territory;
+        this.cityCenterOfRegion = new HashMap<>();
         this.turn = turn;
+        this.currentPlayer = player1;
+        this.player1 = player1;
+        this.player2 = player2;
+        //getCityCenters();
+        //cityCrew = cityCenterOfRegion.get(currentPlayer);
     }
 
     @Override
-    public void beingTurn() {
+    public void StartTurn() {
         getCityCenters();
         cityCrew = cityCenterOfRegion.get(currentPlayer);
     }
@@ -28,7 +37,7 @@ public class TurnConfig implements Turn{
     @Override
     public void endTurn() {
         if(currentPlayer == player1){
-            //currentPlayer == nextPlayer;
+            currentPlayer = player2;
         }else{
             currentPlayer = player1;
             interestProcess();
