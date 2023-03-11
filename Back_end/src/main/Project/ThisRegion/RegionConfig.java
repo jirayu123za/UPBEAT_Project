453af6118d662bc711918bafb9dc79110d4021e4
jirayu <jirayu004za@gmail.com>
2023-03-11 22:02:26 +0700

    update constructor RegionConfig and update methods

--- Back_end/src/main/Project/ThisRegion/RegionConfig.java
+++ Back_end/src/main/Project/ThisRegion/RegionConfig.java
@@ -1,17 +1,18 @@
 package Project.ThisRegion;
-
-import Project.ThisPlayer.Player;
+import Project.ThisPlayer.*;
 
 public class RegionConfig implements Region{
     protected Player owner;
+    protected Position location;
     protected boolean isCityCenter;
     protected long max_Deposit;
     protected long deposit;
 
-    public RegionConfig(long max_Deposit){
+    public RegionConfig(Position location, long max_Deposit){
+        this.location = location;
+        this.max_Deposit = max_Deposit;
         this.owner = null;
         this.isCityCenter = false;
-        this.max_Deposit = max_Deposit;
         this.deposit = 0;
     }
 
@@ -21,8 +22,29 @@ public class RegionConfig implements Region{
     }
 
     @Override
+    public void updateOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    @Override
     public boolean isCityCenter() {
         return isCityCenter;
+    }
+
+    @Override
+    public void setCityCenter(Player owner){
+        this.isCityCenter = true;
+        this.owner = owner;
+    }
+
+    @Override
+    public void changeCityCenter() {
+        isCityCenter = false;
+    }
+
+    @Override
+    public Position getLocation() {
+        return this.location;
     }
 
     @Override
@@ -31,20 +53,9 @@ public class RegionConfig implements Region{
     }
 
     @Override
-    public void updateOwner(Player owner) {
-        this.owner = owner;
-    }
-
-    @Override
     public void updateDeposit(long amount) {
         deposit = Math.max(0, amount + deposit);
         deposit = Math.min(max_Deposit, deposit);
-    }
-
-    @Override
-    public void setCityCenter(Player owner) {
-        isCityCenter = true;
-        updateOwner(owner);
     }
 
     /*
