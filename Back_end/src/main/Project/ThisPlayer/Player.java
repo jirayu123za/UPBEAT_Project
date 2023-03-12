2fcd32118dc05905a62d548307c67da750e45e9d
jirayu <jirayu004za@gmail.com>
2023-03-12 19:33:10 +0700

    update interface for defined methods identifiers

--- Back_end/src/main/Project/ThisPlayer/Player.java
+++ Back_end/src/main/Project/ThisPlayer/Player.java
@@ -1,9 +1,12 @@
 package Project.ThisPlayer;
 
+import java.util.Map;
+
 public interface Player {
     int getID();
     String getName();
     long getBudget();
     boolean updateBudget(long amount);
+    Map<String, Long> identifiers();
 
 }
