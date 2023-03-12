1cb2531a033afa1a79096f46d82316f81e82c051
jirayu <jirayu004za@gmail.com>
2023-03-12 19:35:03 +0700

    implement methods identifiers from modify interface

--- Back_end/src/main/Project/ThisPlayer/PlayerConfig.java
+++ Back_end/src/main/Project/ThisPlayer/PlayerConfig.java
@@ -1,10 +1,13 @@
 package Project.ThisPlayer;
 
+import java.util.Map;
+
 public class PlayerConfig implements Player{
     // Variable
     protected int id;
     protected String name;
     protected long budget;
+    protected Map<String, Long> identifier;
 
     public PlayerConfig(int id, String name, long budget){
         this.id = id;
@@ -33,6 +36,12 @@ public class PlayerConfig implements Player{
         budget = Math.max(0, budget + amount);
         return totalBudget;
     }
+
+    @Override
+    public Map<String, Long> identifiers() {
+        return identifier;
+    }
+
 
     @Override
     public String toString(){
