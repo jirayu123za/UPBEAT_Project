1de834f77833bbed80ea285f8762b58c820bd70f
jirayu <jirayu004za@gmail.com>
2023-03-15 06:02:29 +0700

    refactor name and add testcase but not coverage, need to add more case

--- Back_end/src/main/Project/parseEvaluator/ProcessParseTest.java
+++ Back_end/src/main/Project/parseEvaluator/ProcessParseTest.java
@@ -1,91 +1,71 @@
 package Project.parseEvaluator;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import Project.FileHelpRead;
+import Project.Nodes.*;
+import org.junit.jupiter.api.*;
+import java.io.*;
+import java.util.*;
 
 public class ProcessParseTest {
     public ProcessParse parser;
-    //public ExecuteNode executeNode;
+    public ExecuteNode execute;
+    protected FileHelpRead fileHelpRead;
 
-    /*
     @Test
-    public void Test_sample_construction_plan() {
-        ConstructionPlanCombiner combiner = new ConstructionPlanCombiner();
-        String[] fileNames = {"Back_end/src/constructionPlanCode.txt"};
-        String combinedContent = combiner.combineFiles(fileNames);
-        parser = new GrammarTokenizer(combinedContent);
-        ProcessParse parseEvaluator = new ProcessParse(tokenizer);
+    public void testReadFileConstructionPlan() {
+        String filePath = "Back_end\\src\\constructionPlanCode.txt";
         try {
-            List<ExecuteNode> doState = parseEvaluator.parsePlan();
+            String content = FileHelpRead.readFile(filePath);
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            // Perform any additional validation, e.g., checking the number of nodes
+            Assertions.assertNotNull(content, "Result should not be null");
+            Assertions.assertFalse(content.isEmpty(), "Result should not be empty");
+        } catch (IOException e) {
+            Assertions.fail("Error reading the file: " + e.getMessage());
+        } catch (RuntimeException e) {
+            Assertions.fail("Exception occurred while reading the file: " + e.getMessage());
         }
     }
-     */
+
     @Test
-    public void testMultipleStatement() {
-        parser = new ProcessParse(new GrammarTokenizer(
-     "t = 0 + 1  # keeping track of the turn number\n" + // t ??
-             "m = 0  # number of random moves\n" +
-             "while (deposit) { # still our region\n" +
-             "  if (deposit - 100)\n" +
-             "  then collect (deposit / 4)  # collect 1/4 of available deposit\n" +
-             "  else if (budget - 25) then invest 25\n" +
-             "  else {}\n" +
-             "  if (budget - 100) then {} else done  # too poor to do anything else\n" +
-             "  opponentLoc = opponent\n" +
-             "  if (opponentLoc / 10 - 1)\n" +
-             "  then  # opponent afar\n" +
-             "    if (opponentLoc % 10 - 5) then move downleft\n" +
-             "    else if (opponentLoc % 10 - 4) then move down\n" +
-             "    else if (opponentLoc % 10 - 3) then move downright\n" +
-             "    else if (opponentLoc % 10 - 2) then move downright\n" + // try to correct
-             "    else if (opponentLoc % 10 - 1) then move upright\n" +
-             "    else move up\n" +
-             "  else if (opponentLoc)\n" +
-             "  then  # opponent adjacent to city crew\n" +
-             "    if (opponentLoc % 10 - 5) then {\n" +
-             "      cost = 10 ^ (nearby upleft % 100 + 1)\n" +
-             "      if (budget - cost) then shoot upleft cost else {}\n" +
-             "    }\n" +
-             "    else if (opponentLoc % 10 - 4) then {\n" +
-             "      cost = 10 ^ (nearby downleft % 100 + 1)\n" +
-             "      if (budget - cost) then shoot downleft cost else {}\n" +
-             "    }\n" +
-             "    else if (opponentLoc % 10 - 3) then {\n" +
-             "      cost = 10 ^ (nearby down % 100 + 1)\n" +
-             "      if (budget - cost) then shoot down cost else {}\n" +
-             "    }\n" +
-             "    else if (opponentLoc % 10 - 2) then {\n" +
-             "      cost = 10 ^ (nearby downright % 100 + 1)\n" +
-             "      if (budget - cost) then shoot downright cost else {}\n" +
-             "    }\n" +
-             "    else if (opponentLoc % 10 - 1) then {\n" +
-             "      cost = 10 ^ (nearby upright % 100 + 1)\n" +
-             "      if (budget - cost) then shoot upright cost else {}\n" +
-             "    }\n" +
-             "    else {\n" +
-             "      cost = 10 ^ (nearby up % 100 + 1)\n" +
-             "      if (budget - cost) then shoot up cost else {}\n" +
-             "    }\n" +
-             "  else {  # no visible opponent; move in a random direction\n" +
-             "    dir = random % 6\n" +
-             "    if (dir - 4) then move upleft\n" +
-             "    else if (dir - 3) then move downleft\n" +
-             "    else if (dir - 2) then move down\n" +
-             "    else if (dir - 1) then move downright\n" +
-             "    else if (dir) then move upright\n" +
-             "    else move up\n" +
-             "    m = m + 1\n" +
-             "  }\n" +
-             "}  # end while\n" +
-             "# city crew on a region belonging to nobody, so claim it\n" +
-             "if (budget - 1) then invest 1 else {}\n"
-        ));
-        assertDoesNotThrow(parser::parse);
+    public void testReadFileThenParse() {
+        String filePath = "Back_end\\src\\constructionPlanCode.txt";
+        try {
+            String content = FileHelpRead.readFile(filePath);
+            GrammarTokenizer tokenizer = new GrammarTokenizer(content);
+            ProcessParse parser = new ProcessParse(tokenizer);
+            List<ExecuteNode> result = parser.parse();
+
+            Assertions.assertNotNull(result, "Result should not be null");
+            Assertions.assertFalse(result.isEmpty(), "Result should not be empty");
+            //System.out.println(result);
+        } catch (IOException e) {
+            Assertions.fail("Error reading the file: " + e.getMessage());
+        } catch (RuntimeException e) {
+            Assertions.fail("Parsing error: " + e.getMessage());
+        }
     }
 
+    @Test
+    public void testParseExpression() {
+        String input = "1 + 2 * (3 - 4) / 5 ^ 6";
+        GrammarTokenizer tokenizer = new GrammarTokenizer(input);
+        ProcessParse parser = new ProcessParse(tokenizer);
+        Assertions.assertThrows(SyntaxError.Command404.class, parser::parse);
+    }
 
+    @Test
+    public void testParseWrongWord() {
+        String input = "money";
+        GrammarTokenizer tokenizer = new GrammarTokenizer(input);
+        ProcessParse parser = new ProcessParse(tokenizer);
+        Assertions.assertThrows(SyntaxError.Command404.class, parser::parse, "Expected a syntax error due to the wrong word");
+    }
 
+    @Test
+    public void testParseReservedWord() {
+        String input = "downleft";
+        GrammarTokenizer tokenizer = new GrammarTokenizer(input);
+        ProcessParse parser = new ProcessParse(tokenizer);
+        Assertions.assertThrows(SyntaxError.ReservedWord.class, parser::parse);
+    }
 }
