5f0aeae5b9b83dda6e3eacd677dd3c5f0435a316
jirayu <jirayu004za@gmail.com>
2023-03-16 23:31:18 +0700

    add testcase empty statement, add testcase for text constructionPlan

--- Back_end/src/main/Project/parseEvaluator/ProcessParseTest.java
+++ Back_end/src/main/Project/parseEvaluator/ProcessParseTest.java
@@ -6,9 +6,7 @@ import java.io.*;
 import java.util.*;
 
 public class ProcessParseTest {
-    public ProcessParse parser;
-    public ExecuteNode execute;
-    protected FileHelpRead fileHelpRead;
+    protected ProcessParse parser;
 
     @Test
     public void testReadFileConstructionPlan() {
@@ -43,6 +41,78 @@ public class ProcessParseTest {
         } catch (RuntimeException e) {
             Assertions.fail("Parsing error: " + e.getMessage());
         }
+    }
+
+    @Test
+    public void testParseMultiStatement(){
+        // try to correct
+        String plan = """
+                while (deposit) { # still our region
+                  if (deposit - 100)
+                  then collect (deposit / 4)  # collect 1/4 of available deposit
+                  else if (budget - 25) then invest 25
+                  else {}
+                  if (budget - 100) then {} else done  # too poor to do anything else
+                  opponentLoc = opponent
+                  if (opponentLoc / 10 - 1)
+                  then  # opponent afar
+                    if (opponentLoc % 10 - 5) then move downleft
+                    else if (opponentLoc % 10 - 4) then move down
+                    else if (opponentLoc % 10 - 3) then move downright
+                    else if (opponentLoc % 10 - 2) then move downright
+                    else if (opponentLoc % 10 - 1) then move upright
+                    else move up
+                  else if (opponentLoc)
+                  then  # opponent adjacent to city crew
+                    if (opponentLoc % 10 - 5) then {
+                      cost = 10 ^ (nearby upleft % 100 + 1)
+                      if (budget - cost) then shoot upleft cost else {}
+                    }
+                    else if (opponentLoc % 10 - 4) then {
+                      cost = 10 ^ (nearby downleft % 100 + 1)
+                      if (budget - cost) then shoot downleft cost else {}
+                    }
+                    else if (opponentLoc % 10 - 3) then {
+                      cost = 10 ^ (nearby down % 100 + 1)
+                      if (budget - cost) then shoot down cost else {}
+                    }
+                    else if (opponentLoc % 10 - 2) then {
+                      cost = 10 ^ (nearby downright % 100 + 1)
+                      if (budget - cost) then shoot downright cost else {}
+                    }
+                    else if (opponentLoc % 10 - 1) then {
+                      cost = 10 ^ (nearby upright % 100 + 1)
+                      if (budget - cost) then shoot upright cost else {}
+                    }
+                    else {
+                      cost = 10 ^ (nearby up % 100 + 1)
+                      if (budget - cost) then shoot up cost else {}
+                    }
+                  else {  # no visible opponent; move in a random direction
+                    dir = random % 6
+                    if (dir - 4) then move upleft
+                    else if (dir - 3) then move downleft
+                    else if (dir - 2) then move down
+                    else if (dir - 1) then move downright
+                    else if (dir) then move upright
+                    else move up
+                  }
+                }  # end while
+                # city crew on a region belonging to nobody, so claim it
+                if (budget - 1) then invest 1 else {}
+                """
+        ;
+        GrammarTokenizer tokenizer = new GrammarTokenizer(plan);
+        ProcessParse parse = new ProcessParse(tokenizer);
+        Assertions.assertDoesNotThrow(parse::parse);
+    }
+
+    @Test
+    public void testParseEmptyStatement() {
+        parser = new ProcessParse(new GrammarTokenizer(null));
+        Assertions.assertThrows(SyntaxError.StateRequire.class, parser::parse);
+        parser = new ProcessParse(new GrammarTokenizer(""));
+        Assertions.assertThrows(SyntaxError.StateRequire.class, parser::parse);
     }
 
     @Test
