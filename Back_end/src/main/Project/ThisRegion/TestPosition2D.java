6113adab919bd210117c11d4da6a2d6fc7fe7e48
jirayu <jirayu004za@gmail.com>
2023-03-17 16:08:50 +0700

    update testcase testEquality

--- Back_end/src/main/Project/ThisRegion/TestPosition2D.java
+++ Back_end/src/main/Project/ThisRegion/TestPosition2D.java
@@ -25,12 +25,12 @@ public class TestPosition2D {
     @Test
     public void testCreation(){
         Assertions.assertEquals(2, position.getPosX());
-        Assertions.assertEquals(2, position.getPosY());
+        Assertions.assertEquals(1, position.getPosY());
     }
 
     @Test
     public void testEquality(){
-        Position samePosition = Position.of(2, 2);
+        Position samePosition = Position.of(2, 1);
         Assertions.assertEquals(position, samePosition);
         Position differentPosition = Position.of(3, 3);
         Assertions.assertNotEquals(position, differentPosition);
@@ -50,19 +50,15 @@ public class TestPosition2D {
     public void testIsValidPosition(){
         long rows = 5;
         long cols = 5;
-
         // Test for a position inside the grid boundaries
         Position insideGrid = Position.of(2, 2);
         Assertions.assertTrue(insideGrid.Check_isValidPosition(rows, cols));
-
         // Test for a position with negative coordinates
         Position negativeCoordinates = Position.of(-1, -1);
         Assertions.assertFalse(negativeCoordinates.Check_isValidPosition(rows, cols));
-
         // Test for a position with coordinates exceeding the grid size
         Position outOfBounds = Position.of(6, 6);
         Assertions.assertFalse(outOfBounds.Check_isValidPosition(rows, cols));
-
         // Test for a position on the grid boundaries
         Position onBoundary = Position.of(4, 4);
         Assertions.assertTrue(onBoundary.Check_isValidPosition(rows, cols));
